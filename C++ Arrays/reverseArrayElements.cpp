32a2516e1868686c81090d0b2f2ee533df78083f
NetPriest <65949166+NetPriest@users.noreply.github.com>
2022-06-16 11:56:40 +0300

    Update reverseArrayElements.cpp

--- C++ Arrays/reverseArrayElements.cpp
+++ C++ Arrays/reverseArrayElements.cpp
@@ -5,18 +5,21 @@ using namespace std;
 
 int main(){
 	const int e=11;
-	int arr[e]={10,9,8,7,6,5,4,3,2,1,0};
+	int arr[e]={10,9,8,7,6,5,4,3,2,1};
 	
-	for(int i=0;i<e/2;i++){
-		
+	for(int i=0;i<e/2;i++)
+	{
 		int revInx=e-i-1;
 		int currInx=arr[i];
 		arr[i]=arr[revInx];
-		arr[revInx]=currInx;
-		
+		arr[revInx]=currInx;	
 		//we could swap the above with
 		//swap(arr[i],arr[revInx]);
-		cout<<"arr now is: "<<arr[i]<<endl;
+	}
+	
+	for(int i=0;i<e;i++)
+	{
+		cout<<arr[i]<<endl;
 	}
 	return 0;
 }
